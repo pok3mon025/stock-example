e5a34e20a6520408aad0faaadbd12b42ae924223
pok3mon025 <pok3mon025@gmail.com>
2023-04-13 18:00:31 +0900

    Optimistic Lock 활용해보기

--- src/main/java/com/example/stock/repository/StockRepository.java
+++ src/main/java/com/example/stock/repository/StockRepository.java
@@ -1,10 +1,10 @@
 package com.example.stock.repository;
 
 import com.example.stock.domain.Stock;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.persistence.LockModeType;
 
@@ -14,4 +14,7 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
     @Query("select s from Stock s where s.id=:id")
     Stock findByIdWithPessimisticLock(@Param("id") Long id);
 
+    @Lock(value = LockModeType.OPTIMISTIC)
+    @Query("select s from Stock s where s.id=:id")
+    Stock findByIdWithOptimisticLock(@Param("id") Long id);
 }
